b55e25997ff79c622189d17c3bde1839f4030bcb
Mehrdad <mehrdadkml92@gmail.com>
2023-08-05 23:57:01 +0330

    config DRF
    set up API for server

--- djchat/djchat/urls.py
+++ djchat/djchat/urls.py
@@ -1,7 +1,13 @@
-
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+from server.views import ServerListViewSet
+
+router = DefaultRouter()
+router.register("api/server/select/", ServerListViewSet)
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path("admin/", admin.site.urls),
+]+router.urls
+
--- djchat/server/models.py
+++ djchat/server/models.py
@@ -30,3 +30,4 @@ class Channel(models.Model):
 
     def __str__(self):
         return self.name
+
