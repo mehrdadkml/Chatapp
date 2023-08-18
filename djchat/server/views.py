caaf5759ee90f48bf4bcfff4f698e099fd19b8f1
Mehrdad <mehrdadkml92@gmail.com>
2023-08-19 01:20:21 +0330

    config number of members

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.db.models import Count
+
 from rest_framework import viewsets
 from rest_framework.exceptions import AuthenticationFailed, ValidationError
 from rest_framework.response import Response
@@ -15,9 +16,13 @@ class ServerListViewSet(viewsets.ViewSet):
         qty = request.query_params.get("qty")
         by_user = request.query_params.get("by_user") == "true"
         by_serverid = request.query_params.get("by_serverid")
+        with_num_members = request.query_params.get("with_num_members") == "true"
 
         if by_user or by_serverid and not request.user.is_authenticated:
             raise AuthenticationFailed()
+
+        if with_num_members:
+            self.queryset = self.queryset.annotate(num_members=Count("member"))
 
         if category:
             self.queryset = self.queryset.filter(category__name=category)
@@ -37,5 +42,5 @@ class ServerListViewSet(viewsets.ViewSet):
             except ValueError:
                 raise ValidationError(detail=f"server with id{by_serverid}not found")
 
-        serializer = ServerSerializer(self.queryset, many=True)
+        serializer = ServerSerializer(self.queryset, many=True, context={"num_members": with_num_members})
         return Response(serializer.data)
