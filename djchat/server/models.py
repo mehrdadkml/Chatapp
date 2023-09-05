eac7a478d068fbba6185cdfc85df119767a7f3d8
Mehrdad <mehrdadkml92@gmail.com>
2023-09-06 01:58:29 +0330

    icon config

--- djchat/server/models.py
+++ djchat/server/models.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from django.db import models
+from django.dispatch import receiver
+from django.shortcuts import get_list_or_404
 
 
 def category_icon_upload_path(instance, filename):
@@ -10,6 +12,21 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     descraption = models.TextField(blank=True, null=True)
     icon = models.FileField(upload_to=category_icon_upload_path, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.id:
+            existing = get_object_or_404(Category, id=self.id)
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+        super(Category, self).save(*args, **kwargs)
+
+    @receiver(models.signals.pre_delete, sender="server.Category")
+    def category_delete_files(sender, instance, **kwargs):
+        for field in instance._metaa.fields:
+            if field.name == "icon":
+                file = getattr(instance, field.name)
+                if file:
+                    file.delete(save=False)
 
     def __str__(self):
         return self.name
